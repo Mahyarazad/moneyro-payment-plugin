ca89a63cf8a5ea5d1fc723afd1d899a01bc70839
MaahyarAzad <mhyrinc@gmail.com>
2025-03-11 14:58:34 +0400

    remove redundant js files

--- src/assets/custom-ajax.js
+++
@@ -1,5 +0,0 @@
-jQuery(document).ready(function($) {
-    console.log('Moneyro.js is loaded');
-    console.log('Gateway ID:', moneyro_vars.gateway_id);
-    console.log('Get Rate API:', moneyro_vars.getrate_api);
-});
